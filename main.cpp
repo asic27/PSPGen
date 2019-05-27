290745c7e66eb80c579a1badbab69d23317a46da
asic27 <47812402+asic27@users.noreply.github.com>
2019-05-27 19:21:27 +0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -2,26 +2,15 @@
 #include <iomanip>
 int main(int argc, char **argv)
 {
-    cout<<"Правила ввода"<<endl;
-    cout<<setw(80)<<"1.Начальное заполнение регистра-это любое положительное целое число,кроме 0\n2.Полином вводится в формате x^64+x^32+x^2.Полином обязательно должен содержать элемент в степени 64(старшая степень),который должен стоять первым.\n";
-    cout<<setw(80)<<"3.Длина последовательности-это целое положительное число."<<endl;
-    cout<<"Задайте начальное заполнение регистра: ";
-    int64_t seed=0;
-    unsigned int n=0;
-    cin>>seed;
+    int64_t seed=atoi(argv[1]);
+    unsigned int n=atoi(argv[3]);
     try {
         LFSR a (seed);
-        string path;
-        string poli;
+        string path=argv[4];
+        string poli=argv[2];
         uint64_t chislo=0;
-        cout<<"Введите полином: ";
-        cin>>poli;
         chislo=a.Polynomial_Converter(poli);
         ofstream fout;
-        cout<<"Введите длину генирируемой последовательности: ";
-        cin>>n;
-        cout<<"Введите путь к файлу: ";
-        cin>>path;
         fout.open(path);
         if(!fout.is_open())
             throw LFSR_error(string("Неверный путь к файлу"));
